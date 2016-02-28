6e39e477a7f15efdce63dcd9745fd5fdbd0c489d
TTFlyzebra <852184385@qq.com>
2016-02-29 01:00:48 +0800

    5

---
+++ app/src/main/java/com/flyzebra/xinyi/MyApp.java
@@ -0,0 +1,13 @@
+package com.flyzebra.xinyi;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/2/29.
+ */
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
